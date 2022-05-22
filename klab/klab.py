4b9dc3ec12a4a8cbe11657009ce1255dcacb5655
Ferdinando Villa <ferdinando.villa@gmail.com>
2022-05-22 23:43:00 +0200

    Moving on

--- klab/klab.py
+++ klab/klab.py
@@ -1,24 +1,8 @@
 from enum import Enum
-from klab.engine import Engine
+from engine import Engine
 
 API_BASE = "/api/v2",
 PUBLIC_BASE = API_BASE + "/public",
-
-P_EXPORT = "{export}";
-P_CONTEXT = "{context}";
-P_OBSERVATION = "{observation}";
-P_TICKET = "{ticket}";
-P_ESTIMATE = "{estimate}";
-
-
-class Endpoints(Enum):
-    AUTHENTICATE_USER = API_BASE + "/users/log-in",
-    DEAUTHENTICATE_USER = API_BASE + "/users/log-out",
-    CREATE_CONTEXT = PUBLIC_BASE + "/submit/context",
-    OBSERVE_IN_CONTEXT = PUBLIC_BASE + "/submit/observation/" + P_CONTEXT,
-    SUBMIT_ESTIMATE = PUBLIC_BASE + "/submit/estimate/" + P_ESTIMATE,
-    EXPORT_DATA = PUBLIC_BASE + "/export/" + P_EXPORT + "/" + P_OBSERVATION,
-    TICKET_INFO = PUBLIC_BASE + "/ticket/info/" + P_TICKET
 
 
 class Export(Enum):
@@ -44,3 +28,4 @@ class Klab:
 
     def __init__(self, url, username, password):
         self.engine = Engine(url, username, password)
+
