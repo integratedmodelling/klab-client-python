c05869380c846239d090c299e8539f034702aa6b
Dr. Marcel Buchhorn <marcel.buchhorn@vito.be>
2023-05-23 15:34:43 +0200

    test script to retrieve elevation data via remote engine session

---
+++ tests/example_elevation.py
@@ -0,0 +1,91 @@
+""" connection test to ARIES platform
+
+    install client via pip
+    pip install klab-client-py
+
+    setup a YAML file with your credentials and place in your Home folder - filename: aries_access_credentials.yaml
+    Note: currently only file search for Windows implemented
+
+"""
+
+from klab.klab import Klab
+from klab.geometry import GeometryBuilder
+from klab.observable import Observable
+from klab.observation import Observation
+from klab.utils import Export, ExportFormat
+import asyncio
+import yaml
+import os
+
+
+# function to load credentials and remote engine URL
+def load_credentials():
+    """ Helper function to load the credentials to init the klab remote instance"""
+    file_path = os.path.join(os.path.expanduser("~"), r'aries_access_credentials.yaml')
+
+    with open(file_path) as settings:
+        settings_data = yaml.load(settings, Loader=yaml.Loader)
+    return settings_data
+
+
+# create a function to retrieve the needed data --> needed to trigger asyncio
+# TODO: we need convenience functions --> that is too complex
+async def ARIES_request(klab, area_WKT: str, obs_res: str, obs_year: int, observable: str, export_format, export_path):
+    # create the semantic type and geometry/time to init the CONTEXT
+    obs = Observable.create("earth:Region")
+    grid = GeometryBuilder().grid(urn=area_WKT, resolution=obs_res).years(obs_year).build()
+
+    # now submit to the engine to generate the CONTEXT
+    # in CONTEXT (space/time) we then can request a dataset or model
+    ticketHandler = klab.submit(obs, grid)
+    context = await ticketHandler.get()
+
+    # define the observable (dataset or model) and submit to context
+    obsData = Observable.create(observable)
+    ticketHandler = context.submit(obsData)
+    data = await ticketHandler.get()
+
+    # retrieve the dataset and export to disk
+    data.exportToFile(Export.DATA, export_format, export_path)
+
+# ##############################################
+# here starts the work
+# create a new klab client instance
+try:
+    # get the credentials for the remote klab engine
+    remote_engine = load_credentials()
+
+    klab = Klab.create(remoteOrLocalEngineUrl=remote_engine.get('url'),
+                       username=remote_engine.get('username'),
+                       password=remote_engine.get('pass'))
+    url = remote_engine.get("url")
+except:
+    # if no credentials are setup try to access a local klab engine
+    klab = Klab.create()
+    url = 'K.LAB local instance'
+
+if klab.isOnline():
+    print(f'* connection to {url} was successfully established.')
+else:
+    raise EnvironmentError('could not establish connection to the klab instance')
+
+# setup the parameters for the data retrival
+# area geometry here via a WKT
+polygon_WKT = "EPSG:4326 POLYGON((33.796 -7.086, 35.946 -7.086, 35.946 -9.41, 33.796 -9.41, 33.796 -7.086))"
+observable = "geography:Elevation"
+observation_resolution = "1 km"
+observation_time = 2010
+
+export_format = ExportFormat.GEOTIFF_RASTER
+out_path = os.path.normpath(r'./elevation_test.tif')
+
+# now we can run the asyncio function and execute the data retrival
+try:
+    asyncio.run(ARIES_request(klab, area_WKT=polygon_WKT, observable=observable,
+                              obs_res=observation_resolution, obs_year=observation_time,
+                              export_format=export_format, export_path=out_path))
+    print(f'{observable} dataset successfully retrieved. File saved: {out_path}')
+except:
+    raise RuntimeError('could not retrieve the data from ARIES')
+finally:
+    klab.close()
